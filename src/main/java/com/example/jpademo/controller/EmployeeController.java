182fc9dca1aec96d693bc78fec8a95662d5b7eae
developerworks <developerworks@163.com>
2017-12-15 16:08:36 +0800

    How to use SELECT clause in JPA

--- src/main/java/com/example/jpademo/controller/EmployeeController.java
+++ src/main/java/com/example/jpademo/controller/EmployeeController.java
@@ -17,9 +17,18 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Tuple;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 参考资料
- *
+ * <p>
  * - https://stackoverflow.com/questions/12168624/pagination-response-payload-from-a-restful-api
  * - https://segmentfault.com/q/1010000000603084/a-1020000000635842
  * - http://blog.csdn.net/ie8848520/article/details/8161986
@@ -42,6 +51,9 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    EntityManager entityManager;
 
     @GetMapping(path = "/{id}")
     @ResponseBody
@@ -94,5 +106,152 @@ public class EmployeeController {
         }
 
         return responseEntity;
+    }
+
+    /**
+     * 但行单列
+     * <p>
+     * SELECT uuid FROM employee WHERE uuid = :uuid
+     *
+     * @param uuid
+     * @return
+     */
+    @GetMapping(path = "/single-value-expression")
+    public ResponseEntity<String> singleExpression(@Param("uuid") String uuid) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        // 单值表达式, 因为返回的只是 name 属性, 而不是一个 员工对象
+        CriteriaQuery<String> criteriaQuery = criteriaBuilder.createQuery(String.class);
+        Root<Employee> employeeRoot = criteriaQuery.from(Employee.class);
+        // 投影
+        criteriaQuery.select(employeeRoot.<String>get("uuid"));
+
+        // WHERE 条件
+        List<Predicate> predicates = new ArrayList<>();
+        if (StringUtils.isNoneBlank(uuid)) {
+            // 参数表达式
+            ParameterExpression<String> expression = criteriaBuilder.parameter(String.class, "uuid");
+            predicates.add(criteriaBuilder.equal(employeeRoot.get("uuid"), expression));
+        }
+        if (predicates.size() == 0) {
+            throw new RuntimeException("需要参数UUID");
+        } else if (predicates.size() == 1) {
+            criteriaQuery.where(predicates.get(0));
+        } else {
+            criteriaQuery.where(criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()])));
+        }
+
+        //
+        TypedQuery<String> q = entityManager.createQuery(criteriaQuery);
+        // 设置命名参数
+        // SELECT name FROM employee WHERE uuid = :uuid
+        q.setParameter("uuid", uuid);
+        String s = q.getSingleResult();
+
+        return new ResponseEntity<>(s, HttpStatus.OK);
+    }
+
+    /**
+     * 单行多列: 查询结果为元组方式
+     * <p>
+     * SELECT uuid, name FROM employee WHERE uuid = :uuid
+     *
+     * @param uuid
+     * @return
+     */
+    @GetMapping(path = "/multiple-columns-expression-tuple")
+    public ResponseEntity<Map<String, Object>> multipleColumnsExpressionTuple(@Param("uuid") String uuid) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+        // 查询结果为元组类型
+        CriteriaQuery<Tuple> query = cb.createTupleQuery();
+
+        Root<Employee> employeeRoot = query.from(Employee.class);
+        // 投影
+        query.select(cb.tuple(
+            employeeRoot.<String>get("uuid").alias("uuid"),
+            employeeRoot.<String>get("name").alias("name")
+        ));
+        // WHERE 条件
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (StringUtils.isNoneBlank(uuid)) {
+            ParameterExpression<String> expression = cb.parameter(String.class, "uuid");
+            predicates.add(cb.equal(employeeRoot.get("uuid"), expression));
+        }
+        if (predicates.size() == 0) {
+            throw new RuntimeException("必须提供查询参数UUID");
+        } else if (predicates.size() == 1) {
+            query.where(predicates.get(0));
+        } else {
+            query.where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
+        }
+
+        TypedQuery<Tuple> q = entityManager.createQuery(query);
+        // 设置命名参数
+        // SELECT name FROM employee WHERE uuid = :uuid
+        q.setParameter("uuid", uuid);
+        Tuple t = q.getSingleResult();
+
+        Map<String, Object> result = new HashMap<>();
+
+        Map<String, Object> by_position = new HashMap<>();
+        by_position.put("description", "通过元组索引访问元组元素");
+        by_position.put("uuid", t.get(0));
+        by_position.put("name", t.get(1));
+
+        Map<String, Object> by_alias = new HashMap<>();
+        by_alias.put("description", "通过别名访问元组元素");
+        by_alias.put("uuid", t.get("uuid"));
+        by_alias.put("name", t.get("name"));
+
+        result.put("by_position", by_position);
+        result.put("by_alias", by_alias);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /**
+     * 单行多列: 查询结果为对象
+     * <p>
+     * SELECT uuid, name FROM employee WHERE uuid = :uuid
+     *
+     * @param uuid
+     * @return
+     */
+    @GetMapping(path = "multiple-columns-expression-object")
+    public ResponseEntity multipleColumnsExpressionObjectArray(@Param("uuid") String uuid) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+        // 查询结果为对象数组对象
+        CriteriaQuery<Object> query = cb.createQuery(Object.class);
+        Root<Employee> employeeRoot = query.from(Employee.class);
+        // 投影
+        query.multiselect(
+            employeeRoot.<String>get("uuid").alias("uuid"),
+            employeeRoot.<String>get("name").alias("name")
+        );
+        // WHERE条件集合
+        List<Predicate> predicates = new ArrayList<>();
+        // 动态条件添加
+        if (StringUtils.isNoneBlank(uuid)) {
+            ParameterExpression<String> expression = cb.parameter(String.class, "uuid");
+            predicates.add(cb.equal(employeeRoot.get("uuid"), expression));
+        }
+        if (predicates.size() == 0) {
+            throw new RuntimeException("必须至少提供查询参数UUID");
+        } else if (predicates.size() == 1) {
+            query.where(predicates.get(0));
+        } else {
+            query.where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
+        }
+
+        TypedQuery<Object> q = entityManager.createQuery(query);
+        // 设置命名参数
+        // SELECT name FROM employee WHERE uuid = :uuid
+        q.setParameter("uuid", uuid);
+        Object object = q.getSingleResult();
+
+        return new ResponseEntity<>(object, HttpStatus.OK);
     }
 }
