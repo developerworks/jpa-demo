7bcd75fbef12dafc5e324f8a1bfa1a0659da1687
developerworks <developerworks@163.com>
2017-12-19 14:16:00 +0800

    Add how to validate entity data when persist to PersistenceContext

--- src/main/java/com/example/jpademo/entity/UserDetailsListener.java
+++ src/main/java/com/example/jpademo/entity/UserDetailsListener.java
@@ -3,6 +3,8 @@ package com.example.jpademo.entity;
 import lombok.extern.java.Log;
 
 import javax.persistence.PostLoad;
+import javax.persistence.PrePersist;
+import javax.validation.ValidationException;
 
 /**
  * 实体监听器, 负责监听实体对象的状态变化,作用可通过POSTMAN请求: http://localhost:10001/userdetails?username=${username}
@@ -13,5 +15,12 @@ public class UserDetailsListener {
     @PostLoad
     public void postLoad(UserDetails userDetails) {
         log.info("从数据库加载实体, UserDetails, ID=" + userDetails.getId() + ", USERNAME=" + userDetails.getUsername());
+    }
+
+    @PrePersist
+    public void validate(UserDetails userDetails) {
+        if (userDetails.getUsername().length() < 6) {
+            throw new ValidationException("The length of username at least 6 chars.");
+        }
     }
 }
